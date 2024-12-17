20fc29f0c433bcbeadc8738f1c5153f19b3f2c16
Norm Matloff <nsmatloff@ucdavis.edu>
2024-12-16 21:37:40 -0800

    working

--- R/pcThresh.R
+++ R/pcThresh.R
@@ -2,10 +2,6 @@
 # simple variant of pc algorithm, but with the big distinction that
 # p-values and testing aren't used; users just set thresholds
 
-# to add:
-
-# declare "output" variables, arrows only lead in, e.g. wageinc
-
 # arguments
 
 # data: a date frame or equivalent, all cols factors
@@ -13,20 +9,62 @@
 # abcThresh: if corr(a,b|c) < this value, then delete ab arc
 
 pcThresh <- function(data,abThresh,abcThresh,
-   myCorr='condinformation', permuteCols=FALSE)
+   myCorrABC='condinformation', 
+   myCorrAB=myCorrABC,
+   outputVars=NULL,
+   inputVars=NULL,
+   permuteCols=FALSE)
 {
-   if (myCorr == 'condinformation') myCorr <- condinformation
+   if (myCorrAB == 'condinformation') myCorrAB <- condinformation
+   if (myCorrABC == 'condinformation') myCorrABC <- condinformation
 
    n <- ncol(data)
 
-   if (permuteCols) data <- data[,sample(1:n,n,replace=FALSE)]
+   varNames <- names(data)
+   if (permuteCols) {
+      newColNums <- sample(1:n,n,replace=FALSE)
+      data <- data[,newColNums]
+      newColNames <- names(data)
+      # the variable varNames[i] is now in 
+      # column newColNums[i]
+   } else {
+     newColNums <- 1:n
+     newColNames <- varNames
+   }
+
+
+
    adj <- matrix(1,nrow=n,ncol=n)
-   adj[row(adj) > col(adj)] <- 0
+   rownames(adj) <- newColNames
+   colnames(adj) <- rownames(adj)
+
+   # e.g. want 'wageinc' to only have arrows into it, not out
+   if (!is.null(outputVars)) {
+      # these nodes should not to anything 
+      adj[outputVars,] <- 0
+   }
+
+   # want "causation" to make sense, e.g. gender may cause occupation
+   # but not vice versa
+   if (!is.null(outputVars)) {
+      # these nodes should not to anything 
+      adj[,inputVars] <- 0
+   }
+
+   # want to have no two-way links
+   for (i in 1:(n-1)) {
+      if (!(i %in% outputVars)) {
+         for (j in (i+1):n) {
+            if (runif(1) < 0.5) adj[i,j] <- 0
+            else adj[j,i] <- 0
+         }
+      }
+   }
 
    # delete unconditionally weak links
    for (i in 1:n) 
       for (j in setdiff(1:n,i))  {
-         tmp <- myCorr(data[,i],data[,j])
+         tmp <- myCorrAB(data[,i],data[,j])
          if (tmp < abThresh) adj[i,j] <- 0
       }
 
@@ -35,7 +73,7 @@ pcThresh <- function(data,abThresh,abcThresh,
       for (i in setdiff(1:n,k)) {
          for (j in setdiff(1:n,c(k,i))) {
             if (i < j && adj[k,i] && adj[k,j] && adj[i,j]) {
-               tmp <- myCorr(data[,i],data[,j],data[,k])
+               tmp <- myCorrABC(data[,i],data[,j],data[,k])
                if (tmp < abThresh) adj[i,j] <- 0
             }
          }
@@ -47,5 +85,12 @@ pcThresh <- function(data,abThresh,abcThresh,
 
    adj
 
+}
+
+# forms a list l, such that l[[[i]] consists of the indices in an R
+# factor f for which f == i, the i-th level of f
+rowsByValue <- function(f)
+{
+   lapply(levels(f),function(lvl) which(f == lvl))
 }
 
